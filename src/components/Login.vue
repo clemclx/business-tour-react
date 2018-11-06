9cc9b4a22744e3e4212e708a66ab2648ea22d6c4
alexis <alexis.aubineau@ynov.com>
2018-11-06 14:38:13 +0100

    Adding Login page and Signup page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="container">
-    <h1>Test</h1>
+  <div class="wrapper">
+    <form class="form-signin">
+      <h2 class="form-signin-heading">Authentification</h2>
+      <input type="text" class="form-control" name="username" placeholder="Adresse email" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Mot de passe" required=""/>
+      <label class="checkbox">
+        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Mémoriser
+      </label>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">S'authentifier</button>
+    </form>
   </div>
 </template>
 
@@ -30,5 +38,20 @@ li {
 }
 a {
   color: #42b983;
+}
+input{
+  margin-bottom: 10px;
+}
+.wrapper {
+  margin-bottom: 80px;
+  margin-top: 12%;
+}
+
+.form-signin {
+  max-width: 380px;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);
 }
 </style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="hello">
+  <div class="wrapper">
+    <form class="form-signin">
+      <h2 class="form-signin-heading">Inscription</h2>
+      <input type="text" class="form-control" name="firstname" placeholder="Nom" required="" autofocus="" />
+      <input type="text" class="form-control" name="lastname" placeholder="Prénom" required="" autofocus="" />
+      <input type="text" class="form-control" name="username" placeholder="Pseudonyme" required="" autofocus="" />
+      <input type="text" class="form-control" name="mail" placeholder="Adresse email" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Mot de passe" required=""/>
+      <input type="password" class="form-control" name="password-confirm" placeholder="Confirmer mot de passe" required=""/>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">S'inscrire</button>
+    </form>
   </div>
 </template>
 
@@ -29,5 +39,20 @@ li {
 }
 a {
   color: #42b983;
+}
+input{
+  margin-bottom: 10px;
+}
+.wrapper {
+  margin-bottom: 80px;
+  margin-top: 8%;
+}
+
+.form-signin {
+  max-width: 380px;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);
 }
 </style>
