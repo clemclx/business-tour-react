688ccdc16989effa3d40c9ef56cecb394e68fc44
Alexis <alexis.aubineau@ynov.com>
2018-11-20 11:21:34 +0100

    :sparkles: Waiting vue and Lobby :lipstick: Editing GameBoard

---
+++ src/components/Lobby.vue
@@ -0,0 +1,36 @@
+<template>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom de la partie</th>
+      <th scope="col">Nombre de joueur</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Skewrad</td>
+      <td>1/4</td>
+      <td><router-link to="/waiting" tag="button" class="btn btn-light">Rejoindre</router-link></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Thomas</td>
+      <td>1/4</td>
+      <td><router-link to="/waitting" tag="button" class="btn btn-light">Rejoindre</router-link></td>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style scoped>
+table{
+    margin-top: 5%;
+}
+</style>
--- src/components/Play.vue
+++ src/components/Play.vue
@@ -1,54 +1,70 @@
 <template>
-<div class="table">
-  <div class="board">
-    <div class="col left">
-      <div class="corner"></div>
-      <div class="space"><div class="color group4"><div class="title2 clock"></div><div class="clock clockprice"></div></div></div>
-      <div class="space"><div class="color group4"><div class="title2 clock"></div><div class="clock clockprice"></div></div></div>
-      <div class="space"></div>
-      <div class="space"><div class="color group4"><div class="title2 clock"></div><div class="clock clockprice"></div></div></div>
-      <div class="space"><div class="color group3"><div class="title2 clock clockfix"></div><div class="clock clockprice"></div></div></div>
-      <div class="space"><div class="color group3"><div class="title2 clock clockfix"></div><div class="clock clockprice"></div></div></div>
-      <div class="space"><div class="color group3"><div class="title2 clock"></div><div class="clock clockprice"></div></div></div>
-      <div class="corner"></div>
+  <div class="table">
+    <div>
+      <img src="../assets/img/die-1.svg" alt="Smiley face" height="42" width="42" class="de1"><img src="../assets/img/die-1.svg" alt="Smiley face" height="42" width="42" class="de2">
+      <button class="btn btn-light">Lancer le dé</button>
     </div>
-    <div class="fix">
-      <div class="row top">
-        <div class="space2"><div class="title2 upside"></div><div class="upside upsideprice"></div><div class="color group5"></div></div>
-        <div class="space2"></div>
-        <div class="space2"><div class="title2 upside"></div><div class="upside upsideprice"></div><div class="color group5"></div></div>
-        <div class="space2"><div class="title2 upside"></div><div class="upside upsideprice"></div><div class="color group5"></div></div>
-        <div class="space2"><div class="title2 upside"></div><div class="upside upsideprice"></div><div class="color group6"></div></div>
-        <div class="space2"><div class="title2 upside"></div><div class="upside upsideprice"></div><div class="color group6"></div></div>
-        <div class="space2"><div class="title2 upside"></div><div class="upside upsideprice"></div><div class="color group6"></div></div>
-      </div>
-      <div class="row bottom">
-        <div class="space2"><div class="title"></div><div class="price"></div></div>
-        <div class="space2"><div class="title"></div><div class="price2"></div></div>
-        <div class="space2"></div>
-        <div class="space2"><div class="title"></div><div class="price2"></div></div>
-        <div class="space2"><div class="title"></div><div class="price2"></div></div>
-        <div class="space2"></div>
-        <div class="space2"><div class="title"></div><div class="price"></div></div>
-      </div>
-    </div>
-    <div class="col right">
-      <div class="corner"></div>
-        <div class="space"><div class="color group7"><div class="title2 counter"></div><div class="counter counterprice"></div></div></div>
-        <div class="space"><div class="color group7"><div class="title2 counter"></div><div class="counter counterprice"></div></div></div>
-        <div class="space"><div class="color group7"><div class="title2 counter"></div><div class="counter counterprice"></div></div></div>
-        <div class="space"><div class="color group7"><div class="title2 counter"></div><div class="counter counterprice"></div></div></div>
-        <div class="space"><div class="color group8"><div class="title2 counter" id="park"></div><div class="counter counterprice"></div></div></div>
-        <div class="space"></div>
-        <div class="space"><div class="color group8"><div class="title2 counter" id="boardwalk"></div><div class="counter counterprice"></div></div></div>
+  
+    <div class="board">
+      <div class="col left">
+        <div class="corner"></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
+        <div class="spaceLeft"><singleCase></singleCase></div>
         <div class="corner"></div>
       </div>
+      <div class="fix">
+        <div class="row top">
+          <div class="spaceTop"><singleCase></singleCase></div>
+          <div class="spaceTop"><singleCase></singleCase></div>
+          <div class="spaceTop"><singleCase></singleCase></div>
+          <div class="spaceTop"><singleCase></singleCase></div>
+          <div class="spaceTop"><singleCase></singleCase></div>
+          <div class="spaceTop"><singleCase></singleCase></div>
+          <div class="spaceTop"><singleCase></singleCase></div>
+        </div>
+        <div class="row bottom">
+          <div class="spaceBottom"><singleCase></singleCase></div>
+          <div class="spaceBottom"><singleCase></singleCase></div>
+          <div class="spaceBottom"><singleCase></singleCase></div>
+          <div class="spaceBottom"><singleCase></singleCase></div>
+          <div class="spaceBottom"><singleCase></singleCase></div>
+          <div class="spaceBottom"><singleCase></singleCase></div>
+          <div class="spaceBottom"><singleCase></singleCase></div>
+        </div>
+      </div>
+      <div class="col right">
+        <div class="corner"></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="spaceRight"><singleCase></singleCase></div>
+          <div class="corner"><div class="titleStart">Départ</div></div>
+        </div>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
+import Vue from 'vue';
+Vue.component('singleCase',{
+  data: function(){
+    return {
+      name: 'test',
+      price: '20000$',
+    }
+  },
+  template: '<div><div class="title">{{name}}</div><div class="price">{{price}}</div></div>'
+})
+export default{
+
 }
 </script>
 <style scoped>
@@ -59,7 +75,27 @@ body {
   font-size: 9px;
   text-align: center;
 }
-
+.de1{
+  z-index: 1;
+  transform: translateX(-50%) translateY(-50%);
+  position: absolute;
+  top: 48%;
+  left: 47%;
+}
+.de2{
+  z-index: 1;
+  transform: translateX(-50%) translateY(-50%);
+  position: absolute;
+  top: 48%;
+  left: 51%;
+}
+button{
+  z-index: 1;
+  transform: translateX(-50%) translateY(-50%);
+  position: absolute;
+  top: 55%;
+  left: 49%;
+}
 div {
   color: black;
   margin:0;
@@ -73,11 +109,38 @@ div {
   display: flex;
   flex-direction: row;
   margin-left: 18%;
-  margin-top: 60px
+  margin-top: 60px;
+  z-index: 1;
+  margin-bottom: 60px;
 }
-.space {
-  height: 98px;
-  width: 138px;
+.spaceRight{
+  position: relative; left: 20px; top: -20px; bottom: 60px;
+  height: 138px;
+  width: 98px;
+  border: 1px solid black;
+  background-color: #e5f9e0;
+  transform: rotate(-90deg)
+}
+.spaceLeft{
+  position: relative; left: 20px; top: -20px;; bottom: 60px;
+  height: 138px;
+  width: 98px;
+  border: 1px solid black;
+  background-color: #e5f9e0;
+  transform: rotate(90deg)
+}
+.spaceTop{
+  position: relative;
+  width:98px;
+  height: 128px;
+  border: 1px solid black;
+  background-color: #e5f9e0;
+  transform: rotate(180deg)
+}
+.spaceBottom{
+  position: relative;
+  width:98px;
+  height: 128px;
   border: 1px solid black;
   background-color: #e5f9e0;
 }
@@ -125,97 +188,20 @@ div {
   width: 100%;
   height: 25%;
 }
-.group3 {position:relative;}
-.group4 {position:relative;}
-.group5 {position:relative;}
-.group6 {position:relative;}
-.group7 {position:relative;}
-.group8 {position:relative;}
-.title {
-  padding: 16PX 5PX;
-}
-.title2 {
-  text-align: center;
-}
+.group {position:relative;}
 .price {
   font-size: 0.85em;
-  margin-top: 45px;
-}
-.price2 {
-  font-size: 0.85em;
-  margin-top: 61px;
-}
-.counter {
-  width: 0px;
-  left: 80px;
-  top: 60px;
-  position: absolute;
-  -webkit-transform: rotate(-90deg);
-  -moz-transform: rotate(-90deg);
-  -ms-transform: rotate(-90deg);
-  -o-transform: rotate(-90deg);
-  filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
-}
-.counterprice {
-  font-size: 0.85em;
-  left: 140px;
-  top: 40px;
-  width: 85px;
+  margin-top: 75%;
 }
 .clock {
   width: 0px;
-  right: 80px;
-  top: -5px;
+  right: 20px;
+  top: 20px;
   position: absolute;
   -webkit-transform: rotate(90deg);
   -moz-transform: rotate(90deg);
   -ms-transform: rotate(90deg);
   -o-transform: rotate(90deg);
   filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=1);
-}
-.clockfix {top:5px;}
-.clockprice {
-  font-size: 0.85em;
-  left: -176px;
-  top: 40px;
-  width: 85px;
-}
-.upside {
-  width:0;
-  top: 60px;
-  left: 84px;
-  font-size:14px;
-  position: absolute;
-  -webkit-transform: rotate(180deg);
-  -moz-transform: rotate(180deg);
-  -ms-transform: rotate(180deg);
-  -o-transform: rotate(180deg);
-  filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=2);
-}
-.upsideprice {
-  font-size: 0.85em;
-  left: 10px;
-  top: 10px;
-  width: 55px;
-}
-#park {
-  top: 50px;
-}
-#boardwalk {
-  top:84px;
-}
-#titletext {
-  width:200px;
-  height:100px;
-  font-size: 25px;
-  top: -302px;
-  left: -705px;
-  font-size:104px;
-  position: absolute;
-  -webkit-transform: rotate(-45deg);
-  -moz-transform: rotate(-45deg);
-  -ms-transform: rotate(-45deg);
-  -o-transform: rotate(-45deg);
-  filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3.5);
 }
 </style>
